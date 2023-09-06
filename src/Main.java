766cc78341d43f468a45d14cdc96b8252f0703c9
JSanchez3010 <jonatha.emanuel.sanchez@gmail.com>
2023-09-06 00:04:27 -0300

    tipo de datos

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+      //Non-primitive DATA TYPE
+
+      //String - variable String storage text.
+      String variableString = "Caracteres";
+
+
+      //Primitive DATA TYPES
+
+      //Byte - variable Byte storage numbers (-128 to 128)
+      byte variableByte = (byte) 128;
+
+      //Short - variable Short storage numbers (-32,768 to 3277)
+      short variableShort = (short) 32;
+
+      //Int - variable Integers storage numbers without decimal. (-2,147,483,648 to 2,147,483,47)
+      int variableInt = 123456789;
+
+      //Long - variable Long storage 64-bits two's complement integer. (-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+      long variableLong = 31;
+
+      //Float - variable Float storage numbers with decimals. (Sufficient for storing 6 to 7 decimal digits)
+      float variableFloat = 20.123F;
+
+      //Double - variable Double storage fractional numbers. (Sufficient for storing 15 decimal digits)
+      double variableDouble = 1523434556;
+
+      //Boolean - variable Boolean storage values with two states.
+      boolean variableBoolean = true;
+      boolean variableBooleann = false;
+
+      //Char - variable Char storage single characters. (Stores a single character/letter or ASCII values)
+      char variableChar = 'a';
+
+
+
+
+
+
+    }
+}
